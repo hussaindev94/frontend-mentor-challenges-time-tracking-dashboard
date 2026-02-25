0a411404496d4f8b54dc0f5d9bc0e3a7085b102c
Hussain <hussain.dev94@gmail.com>
2026-02-26 01:39:20 +0300

    Adding one card & checking user action, fetching & graping data functionalities.

--- script.js
+++ script.js
@@ -1,36 +1,66 @@
 //Grap the timeFrameSelector parent
 const timeFrameParent = document.querySelector(".timeframe-selector");
 
-console.log(timeFrameParent);
+//Function to Update the UI.
+function updateUiElements(){}
+//Function to grap the right data.
+function getTimeFrameData(data, timeFrameName) {
+    console.log(timeFrameName);
+    const selectedTimeFrameData = data.map(dataItem => {
+        return {
+            title: dataItem.title,
+            timeFrameData: dataItem.timeframes[timeFrameName]
+        }
+    });
+    //Call the updateUiElement function and pass the returnd data to it:
 
-//Function to check on the timeFrameSelector state:
-function checkOnTimeFrameState(child) {
-    //For futuer updates if the yearly timeFrameSelector has been added, add the "timeFrameSelector" class to it.
+}
 
+
+//function change the selected timeFrame.
+function changeTimeFrame(nextTimeFrame) {
     //Grap the timeFrameSelectors and convert them into array:
     const arrayOfTimeSelectors = [...document.querySelectorAll(".timeFrameSelector")];
+
+    //Loop through the timeFrameSelector and filter them with the one tht has true value.
+    //Change the true into false:
+    arrayOfTimeSelectors.filter(timeFrameSelector => timeFrameSelector.dataset.selected === "true")[0].dataset.selected = "false";
+    nextTimeFrame.dataset.selected = "true";
+
+}
+//Function to check on the timeFrameSelector state:
+function checkOnTimeFrameState(nextTimeFrame) {
+    //For futuer updates if the yearly timeFrameSelector has been added, add the "timeFrameSelector" class to it.
+
 
     //If the user clicked on active timeFrameSelector:
     //Do nothing:
     //If the user clicked on non active timeFrameSelector:
     //Turn off the active one and turn on the non active.
-    if (child.dataset.selected === "true") {
+    if (nextTimeFrame.dataset.selected === "true") {
         return
     } else {
-        //Loop through the timeFrameSelector and filter them with the one tht has true value
-        //Change the true into false:
-        arrayOfTimeSelectors.filter(timeFrameSelector => timeFrameSelector.dataset.selected === "true")[0].dataset.selected = "false";
-        child.dataset.selected = "true";
+        changeTimeFrame(nextTimeFrame);
 
+
+        const nextTimeFrameName = nextTimeFrame.textContent.toLowerCase();
+        //Fetch the data based on the user action: Daily, Weekly or Monthly.
+        fetch("./data.json").then(res => {
+            if (!res.ok) return console.log("No Response")
+
+            return res.json();
+
+        }).then(data => getTimeFrameData(data, nextTimeFrameName)).catch(e => console.log(e));
     }
 
 }
 
 //Event Listener CB function here:
 const handelClick = (event) => {
-    const timeFrameSelector = event.target;
-    checkOnTimeFrameState(timeFrameSelector);
-    //Also fitch the data based on the user action: Daily, Weekly or Monthly.
+
+    const nextTimeFrame = event.target;
+    //console.log(timeFrameSelector.textContent);
+    checkOnTimeFrameState(nextTimeFrame);
 }
 //There is another approch of adding event listener to the childs, but this will consume more memory.
 //Add the Event Listener to the parent:
